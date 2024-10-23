f7576873b3ad798775de79ea97585a573442e2b4
AshrafWael <ashrafwael582@gmail.com>
2024-10-23 19:46:28 +0300

    update Isuue Manager

--- HospitalManagmentSystem.BLL/Manager/IssueManager.cs
+++ HospitalManagmentSystem.BLL/Manager/IssueManager.cs
@@ -1,4 +1,6 @@
-﻿using HospitalManagmentSystem.BLL.Dtos;
+﻿using AutoMapper;
+using HospitalManagmentSystem.BLL.Dtos;
+using HospitalManagmentSystem.DAL.Data.Models;
 using HospitalManagmentSystem.DAL.Reposatories;
 using System;
 using System.Collections.Generic;
@@ -11,40 +13,49 @@ namespace HospitalManagmentSystem.BLL.Manager
     public class IssueManager : IIssueManager
     {
         private readonly IIssueRepo _issueRepo;
+        private readonly IMapper _mapper;
 
-        public IssueManager(IIssueRepo issueRepo)
+        public IssueManager(IIssueRepo issueRepo,IMapper mapper)
         {
             _issueRepo = issueRepo;
+         _mapper = mapper;
         }
 
         public IEnumerable<IssueReadDto> GetAll()
         {
-            throw new NotImplementedException();
+      
+           return _mapper.Map<List<IssueReadDto>>(_issueRepo.GetAll());
 
         }
 
         public IssueReadDto GetById(int id)
         {
-            throw new NotImplementedException();
+            return _mapper.Map<IssueReadDto>(_issueRepo.GetById(id));
         }
 
         public void Add(IssueReadDto issueReadDto)
         {
-            throw new NotImplementedException();
+            var isue = _mapper.Map<Issue>(issueReadDto);
+            _issueRepo.Add(isue);
+            _issueRepo.SaveChanges();
         }
 
         public void Update(IssueUpdateDto issueUpdateDto)
         {
-            throw new NotImplementedException();
+            var IssueModel = _issueRepo.GetById(issueUpdateDto.Id);
+            _mapper.Map<IssueUpdateDto>(IssueModel);
+            _issueRepo.SaveChanges();
         }
         public void Delete(int Id)
         {
-            throw new NotImplementedException();
+          var isue =_issueRepo.GetById(Id);
+            _issueRepo.Delete(isue);
+            _issueRepo.SaveChanges();
         }
 
         public void SaveChanges()
         {
-            throw new NotImplementedException();
+            _issueRepo.SaveChanges();
         }
     }
 }
