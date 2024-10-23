61833180811c3d9e02ac28f783d8cf134aaa1bb0
AshrafWael <ashrafwael582@gmail.com>
2024-10-23 20:10:47 +0300

    Add Craud Operation

--- HospitalManagmentSystem.API/Controllers/PatientsController.cs
+++ HospitalManagmentSystem.API/Controllers/PatientsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HospitalManagmentSystem.BLL.Dtos;
+using HospitalManagmentSystem.BLL.Manager;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalManagmentSystem.API.Controllers
@@ -7,5 +9,64 @@ namespace HospitalManagmentSystem.API.Controllers
     [ApiController]
     public class PatientsController : ControllerBase
     {
+
+
+        private readonly IPatientManager _patientManager;
+
+        public PatientsController(IPatientManager patientManager)
+        {
+            _patientManager = patientManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PatientReadDto>> GetAll()
+        {
+            return Ok(_patientManager.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var patient = _patientManager.GetById(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
+
+        [HttpPost]
+        public ActionResult Add(PatientAddDto patientDto)
+        {
+            _patientManager.Add(patientDto);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, PatientUpdateDto patientDto)
+        {
+            if (id != patientDto.Id)
+            {
+                return BadRequest();
+            }
+
+            _patientManager.Update(patientDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var patient = _patientManager.GetById(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _patientManager.Delete(id);
+            return NoContent();
+        }
+
+
     }
 }
