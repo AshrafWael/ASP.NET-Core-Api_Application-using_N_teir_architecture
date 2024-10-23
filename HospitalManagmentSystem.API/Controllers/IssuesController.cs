bd5836d6455e17db9ee79e33c733e474fddcc581
AshrafWael <ashrafwael582@gmail.com>
2024-10-23 20:10:40 +0300

    Add Craud Operation

--- HospitalManagmentSystem.API/Controllers/IssuesController.cs
+++ HospitalManagmentSystem.API/Controllers/IssuesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HospitalManagmentSystem.BLL.Dtos;
+using HospitalManagmentSystem.BLL.Manager;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalManagmentSystem.API.Controllers
@@ -7,5 +9,62 @@ namespace HospitalManagmentSystem.API.Controllers
     [ApiController]
     public class IssuesController : ControllerBase
     {
+        private readonly IIssueManager _issueManager;
+
+        public IssuesController(IIssueManager issueManager)
+        {
+            _issueManager = issueManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<IssueReadDto>> GetAll()
+        {
+            return Ok(_issueManager.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var issue = _issueManager.GetById(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+            return Ok(issue);
+        }
+
+        [HttpPost]
+        public ActionResult Add(IssueReadDto issueDto)
+        {
+            _issueManager.Add(issueDto);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, IssueUpdateDto issueDto)
+        {
+            if (id != issueDto.Id)
+            {
+                return BadRequest();
+            }
+
+            _issueManager.Update(issueDto);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var issue = _issueManager.GetById(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            _issueManager.Delete(id);
+            return NoContent();
+        }
+
+
     }
 }
